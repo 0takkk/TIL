13ee544937cd695177aa564ce962dc3e35b4cc82
0takkk <syt0719@naver.com>
2025-06-16 23:49:59 +0900

    feat: add sleeping mode

--- OOP/designpatterns/src/state/problem/Client.java
+++ OOP/designpatterns/src/state/problem/Client.java
@@ -1,10 +1,14 @@
 package state.problem;
 
+import state.problem.light.Light;
+
 public class Client {
 
     public static void main(String[] args) {
         Light light = new Light();
         light.off();
+        light.on();
+        light.on();
         light.on();
         light.off();
     }
--- OOP/designpatterns/src/state/problem/light/Light.java
+++ OOP/designpatterns/src/state/problem/light/Light.java
@@ -1,9 +1,10 @@
-package state.problem;
+package state.problem.light;
 
 public class Light {
 
     private static int ON = 0;  // 형광등이 켜진 상태
     private static int OFF = 1;  // 형광등이 꺼진 상태
+    private static int SLEEPING = 2;  // 취침등 상태 추가
 
     private int state;  // 형광등의 현재 상태
 
@@ -13,7 +14,12 @@ public class Light {
 
     public void on() {
         if(state == ON) {
-            System.out.println("반응 없음");
+            System.out.println("취침등 On!");
+            state = SLEEPING;
+        }
+        else if(state == SLEEPING) {
+            System.out.println("Light On!");
+            state = ON;
         }
         else {
             System.out.println("Light On!");
@@ -24,6 +30,10 @@ public class Light {
     public void off() {
         if(state == OFF) {
             System.out.println("반응 없음");
+        }
+        else if (state == SLEEPING) {
+            System.out.println("Light OFF!");
+            state = OFF;
         }
         else {
             System.out.println("Light OFF!");
