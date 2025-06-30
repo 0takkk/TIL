ba07127d557394b4239901d4c6c28e033dcda3db
0takkk <syt0719@naver.com>
2025-06-30 22:33:44 +0900

    feat: add composite pattern solution
    - add speaker

--- OOP/designpatterns/src/composite/solution/Client.java
+++ OOP/designpatterns/src/composite/solution/Client.java
@@ -1,9 +1,6 @@
 package composite.solution;
 
-import composite.solution.computer.Body;
-import composite.solution.computer.Computer;
-import composite.solution.computer.Keyboard;
-import composite.solution.computer.Monitor;
+import composite.solution.computer.*;
 
 public class Client {
 
@@ -11,11 +8,13 @@ public class Client {
         Body body = new Body(100, 70);
         Keyboard keyboard = new Keyboard(5, 2);
         Monitor monitor = new Monitor(50, 30);
+        Speaker speaker = new Speaker(30, 20);
 
         Computer computer = new Computer();
         computer.addDevice(body);
         computer.addDevice(keyboard);
         computer.addDevice(monitor);
+        computer.addDevice(speaker);
 
         System.out.printf("Computer Power = %dW\n", computer.getPower());
         System.out.printf("Computer Price = %d만원", computer.getPrice());
