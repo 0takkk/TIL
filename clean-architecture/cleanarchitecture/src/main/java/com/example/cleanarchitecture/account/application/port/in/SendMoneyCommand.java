abe6d44a3780ed24cf31d42185b8d0cd2b8e70c9
0takkk <syt0719@naver.com>
2025-07-13 19:05:56 +0900

    feat: 입력 유효성 검증

--- clean-architecture/cleanarchitecture/src/main/java/com/example/cleanarchitecture/account/application/port/in/SendMoneyCommand.java
+++ clean-architecture/cleanarchitecture/src/main/java/com/example/cleanarchitecture/account/application/port/in/SendMoneyCommand.java
@@ -1,5 +1,27 @@
 package com.example.cleanarchitecture.account.application.port.in;
 
-public class SendMoneyCommand {
+import com.example.cleanarchitecture.account.domain.Money;
+import com.example.cleanarchitecture.account.domain.vo.AccountId;
+import com.example.cleanarchitecture.common.SelfValidating;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
+@Getter
+public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
+
+    @NotNull
+    private final AccountId sourceAccountId;
+
+    @NotNull
+    private final AccountId targetAccountId;
+
+    @NotNull
+    private final Money money;
+
+    public SendMoneyCommand(AccountId sourceAccountId, AccountId targetAccountId, Money money) {
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.money = money;
+        this.validateSelf();
+    }
 }
